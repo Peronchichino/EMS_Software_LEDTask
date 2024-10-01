0a80f500e1fc8dc29532b2fe4de3232e0000419e
Lukas Buchmayer <lukasbuchmayer@gmail.com>
2024-10-01 12:50:40 +0200

    Add files via upload

---
+++ bsp_rgb_led.h
@@ -0,0 +1,22 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#include <cpu.h>
+#include <rgb_led_blue.h>
+#include <rgb_led_green.h>
+#include <rgb_led_red.h>
+
+#include <PWM_rgb_blue.h>
+#include <PWM_rgb_green.h>
+#include <PWM_rgb_red.h>
+
+/* [] END OF FILE */
